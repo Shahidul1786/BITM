783cfe0c223df1c4546c4f1811a564d105693c56
Shahidul1786 <stshahidislam1786@gmail.com>
2019-06-24 12:32:30 +0600

    Fragment

---
+++ BITMD12Fragment/app/src/main/java/com/batch10/bitm/bitmd12fragment/TwoFragment.java
@@ -0,0 +1,30 @@
+package com.batch10.bitm.bitmd12fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TwoFragment extends Fragment {
+
+
+    public TwoFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        return view;
+    }
+
+}
