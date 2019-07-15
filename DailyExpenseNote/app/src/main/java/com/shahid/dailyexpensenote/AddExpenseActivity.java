4af3da2a343f89918505286de9330c2aa3c68e62
Shahidul1786 <stshahidislam1786@gmail.com>
2019-07-15 16:46:25 +0600

    create design but expense details imcomplete

---
+++ DailyExpenseNote/app/src/main/java/com/shahid/dailyexpensenote/AddExpenseActivity.java
@@ -0,0 +1,14 @@
+package com.shahid.dailyexpensenote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+}
---
+++ DailyExpenseNote/app/src/main/java/com/shahid/dailyexpensenote/ExpenseDatailsActivity.java
@@ -0,0 +1,14 @@
+package com.shahid.dailyexpensenote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpenseDatailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_datails);
+    }
+}
