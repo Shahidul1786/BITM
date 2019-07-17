dab4b1aa41c2f704f8aae234b557c826b9e06545
Shahidul1786 <stshahidislam1786@gmail.com>
2019-07-18 01:07:42 +0600

    OTP complete

--- BITMD22-Firebase-Authentication(OTP)/app/src/main/java/com/shahid/bitmd22_firebase_authenticationotp/LoginActivity.java
+++ BITMD22-Firebase-Authentication(OTP)/app/src/main/java/com/shahid/bitmd22_firebase_authenticationotp/LoginActivity.java
@@ -2,14 +2,18 @@ package com.shahid.bitmd22_firebase_authenticationotp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText phoneNoET;
     private Button nextBtn;
+    private String phoneNo;
     
 
     @Override
@@ -20,7 +24,21 @@ public class LoginActivity extends AppCompatActivity {
         init();
 
 
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               phoneNo = phoneNoET.getText().toString().trim();
 
+               if (phoneNo.length() == 11){
+                   Intent intent = new Intent(LoginActivity.this,VerifyActivity.class);
+                   intent.putExtra("phone",phoneNo);
+                   startActivity(intent);
+               }
+               else {
+                   Toast.makeText(LoginActivity.this, "phone no invalid", Toast.LENGTH_SHORT).show();
+               }
+            }
+        });
 
 
     }
--- BITMD22-Firebase-Authentication(OTP)/app/src/main/java/com/shahid/bitmd22_firebase_authenticationotp/VerifyActivity.java
+++ BITMD22-Firebase-Authentication(OTP)/app/src/main/java/com/shahid/bitmd22_firebase_authenticationotp/VerifyActivity.java
@@ -1,14 +1,127 @@
 package com.shahid.bitmd22_firebase_authenticationotp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class VerifyActivity extends AppCompatActivity {
+
+    private EditText codeET;
+    private Button loginBTN;
+    private String phoneNo;
+    private String verificationId;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
+
+        init();
+
+        mAuth = FirebaseAuth.getInstance();
+        phoneNo = getIntent().getStringExtra("phone");
+
+        sendOTP();
+
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               String code = codeET.getText().toString();
+
+               if (code.length() == 6){
+                   verify(code);
+               }
+            }
+        });
+
+
+    }
+
+    private void sendOTP() {
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+88"+phoneNo,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacksPhoneAuthActivity.java
+    }
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            String code = phoneAuthCredential.getSmsCode();
+
+            if (code != null){
+
+                codeET.setText(code);
+                verify(code);
+
+            }
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+            Toast.makeText(VerifyActivity.this, ""+e.getMessage(), Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            verificationId = s;
+        }
+    };
+
+    private void verify(String code) {
+
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+
+        signInWithPhoneAuthCredential(credential);
+
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                Intent intent = new Intent(VerifyActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    private void init() {
+
+        codeET = findViewById(R.id.codeETID);
+        loginBTN = findViewById(R.id.loginBTNID);
+
+
     }
 }
