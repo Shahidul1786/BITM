54ba11d2530dee01dab2821a7a228147774e9649
Shahidul1786 <stshahidislam1786@gmail.com>
2019-08-01 23:54:55 +0600

    Main activity and dashboard_fragment design

--- TourMate/app/src/main/java/com/shahid/tourmate/MainActivity.java
+++ TourMate/app/src/main/java/com/shahid/tourmate/MainActivity.java
@@ -1,14 +1,35 @@
 package com.shahid.tourmate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.shahid.tourmate.adapters.DashboardAdapter;
+import com.shahid.tourmate.pojoClass.User;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private DashboardAdapter adapter;
+    private List<User> userList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        init();
+    }
+
+    private void init() {
+
+        recyclerView = new RecyclerView(this);
+
     }
 }
---
+++ TourMate/app/src/main/java/com/shahid/tourmate/adapters/DashboardAdapter.java
@@ -0,0 +1,50 @@
+package com.shahid.tourmate.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.shahid.tourmate.R;
+import com.shahid.tourmate.pojoClass.User;
+
+import java.util.List;
+
+public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.ViewHolder> {
+
+    private List<User> userList;
+    private Context context;
+
+    public DashboardAdapter(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_dashboard,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
